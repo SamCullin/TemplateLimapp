1f19872357cc3d7a503acae87c1735fa0731761d
Sam Cullin <sam.cullin@findas.io>
2021-03-27 19:08:18 +1100

    pointcloud testing

---
+++ Assets/ComputeShaders/MultibufferParticles.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+
+public class MultibufferParticles : MonoBehaviour
+{
+	private struct Particle
+	{
+		public Vector3 position;
+		public Vector4 color;
+	}
+
+	private const int SIZE_PARTICLE = 28;
+
+	[SerializeField]
+	private int particleCount = 10000;
+	private int lastParticleCount = 0;
+
+	[SerializeField]
+	private float power = 1;
+
+	[SerializeField]
+	private bool enableFog = false;
+	
+	[SerializeField]
+	private bool enableShadows = false;
+	
+	[SerializeField]
+	private bool enableColor = false;
+	
+	[SerializeField]
+	private bool enableCutOff = false;
+
+
+
+	[SerializeField]
+	public Material material;
+
+	[SerializeField]
+	public ComputeShader computeShader;
+
+	private int mComputeShaderKernelID;
+	private const int WARP_SIZE = 128;
+	private int mWarpCount;
+
+	[SerializeField]
+	private Transform camPos;
+
+	[SerializeField]
+	private float _FOV = 1.0f;
+
+
+	[SerializeField]
+	private float _StepRatio = 1f;
+	public float StepRatio { get{return _StepRatio;} set{_StepRatio = value;} }
+
+	[SerializeField]
+	private float _Threshold = 1f;
+	public float Threshold { get{return _Threshold;} set{_Threshold = value;} }
+
+	[SerializeField]
+	private float _jitter = 1f;
+	
+	[SerializeField]
+	private float _fogAmount = 1f;
+
+
+ 	[ColorUsageAttribute(true,true,0f,8f,0.125f,3f)] 
+	[SerializeField]
+	private Color _Color;
+
+	[SerializeField]
+	private int numBuffers = 8;
+
+	[SerializeField]
+	private int activeBuffer = 0;
+
+
+	private Vector4 _RenderParam
+	{
+		get { return new Vector4(AO1, AO2, AO3, AO4); }
+	}
+
+	[SerializeField]
+	public float AO1 = 1;
+
+	[SerializeField]
+	public float AO2 = 1;
+
+	[SerializeField]
+	public float AO3 = 1;
+
+
+	[SerializeField]
+	public float AO4 = 1;
+
+
+
+	private ComputeBuffer[] particleBuffers;
+
+	void Start()
+	{
+		InitBuffers();
+
+	}
+
+	void OnDestroy()
+	{
+		for(int i = 0; i < particleBuffers.Length; i ++)
+		{
+			if ( particleBuffers[i] != null)
+				particleBuffers[i].Release();
+		}
+	}
+	
+	void Update()
+	{
+		InitBuffers();
+		mComputeShaderKernelID = computeShader.FindKernel("CSMain");
+		computeShader.GetKernelThreadGroupSizes(mComputeShaderKernelID, out uint xGroupSize, out uint yGroupSize, out uint zGroupSize);
+		mWarpCount = Mathf.CeilToInt((float)particleCount / xGroupSize);
+
+
+		material.SetMatrix("modelToWorld", transform.localToWorldMatrix);
+		material.SetColor("_Color", _Color);
+
+		activeBuffer = (activeBuffer + 1) % numBuffers;
+
+		// Send datas to the compute shader
+		computeShader.SetFloat("Time", Time.time / 40f);
+		computeShader.SetFloat("FOV", _FOV);
+		computeShader.SetFloat("pointDim", Mathf.Sqrt(particleCount));
+		computeShader.SetFloat("stepRatio", _StepRatio);
+
+		computeShader.SetBool("enableFog", this.enableFog);
+		computeShader.SetBool("enableShadows", this.enableShadows);
+		computeShader.SetBool("enableColor", this.enableColor);
+		computeShader.SetBool("enableCutOff", this.enableCutOff);
+		
+
+		computeShader.SetVector("camPos",
+		 transform.transform.InverseTransformPoint(camPos.position));
+
+		Quaternion q =  Quaternion.Inverse(transform.rotation) * camPos.rotation;
+
+		computeShader.SetVector("camQRot", new Vector4(q.x, q.y, q.z, q.w));
+		computeShader.SetFloat("scale", transform.localToWorldMatrix.GetScale().x );
+
+		computeShader.SetFloat("_Threshold", _Threshold);
+		computeShader.SetFloat("_StepRatio", _StepRatio);
+		computeShader.SetFloat("_jitter", _jitter);
+		computeShader.SetVector("_RenderParam", _RenderParam);
+		computeShader.SetFloat("_fogAmount", _fogAmount);
+		computeShader.SetFloat("_power", this.power);
+		// Update the Particles, only for the ative buffer
+ 
+		computeShader.SetBuffer(mComputeShaderKernelID, "particleBuffer", particleBuffers[activeBuffer]);
+		computeShader.Dispatch(mComputeShaderKernelID, mWarpCount, 1, 1);
+	}
+
+	void OnRenderObject()
+	{
+		// draw ALL the buffers
+		for ( int i = 0; i < particleBuffers.Length; i++)
+		{
+			material.SetPass(0);
+			material.SetBuffer("particleBuffer", particleBuffers[i]);
+			Graphics.DrawProceduralNow(MeshTopology.Points, 1, particleCount);
+		}
+	}
+	
+
+	void InitBuffers()
+    {
+		if(particleBuffers == null || particleBuffers.Length != numBuffers || particleCount != lastParticleCount)
+        {
+			particleBuffers = new ComputeBuffer[numBuffers];
+			for (int i = 0; i < numBuffers; i++)
+			{
+				Particle[] particleArray = new Particle[particleCount];
+				particleBuffers[i] = new ComputeBuffer(particleCount, SIZE_PARTICLE);
+				particleBuffers[i].SetData(particleArray);
+			}
+			lastParticleCount = particleCount;
+		}
+    }
+}
---
+++ Assets/FractalVRMaster.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+public struct FractalCamera {
+    public Matrix4x4 toWorld;
+    public Matrix4x4 inverseProjection;
+}
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class FractalVRMaster : MonoBehaviour
+{
+
+    public ComputeShader fractalShader;
+    public Camera leftEye;
+    public Camera rightEye;
+
+    [Range(1, 20)]
+    public float fractalPower = 10;
+    public float darkness = 70;
+
+    [Header("Colour mixing")]
+    [Range(0, 1)] public float blackAndWhite;
+    [Range(0, 1)] public float redA;
+    [Range(0, 1)] public float greenA;
+    [Range(0, 1)] public float blueA = 1;
+    [Range(0, 1)] public float redB = 1;
+    [Range(0, 1)] public float greenB;
+    [Range(0, 1)] public float blueB;
+
+    RenderTexture targetLeft;
+    RenderTexture targetRight;
+    Light directionalLight;
+
+    private FractalCamera[] eyes;
+    private ComputeBuffer eyeComputeBuffer;
+    private int fractalKernalId;
+
+    [Header("Animation Settings")]
+    public float powerIncreaseSpeed = 0.2f;
+
+    FractalCamera createFractalCamera(Camera cam)
+    {
+        FractalCamera fCam = new FractalCamera();
+        fCam.toWorld = this.leftEye.cameraToWorldMatrix;
+        fCam.inverseProjection = this.leftEye.projectionMatrix.inverse;
+        return fCam;
+    }
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        directionalLight = FindObjectOfType<Light>();
+        fractalKernalId = this.fractalShader.FindKernel("CSMain");
+
+
+        FractalCamera leftEye = this.createFractalCamera(this.leftEye);
+        FractalCamera rightEye = this.createFractalCamera(this.rightEye);
+
+        this.eyes = new FractalCamera[] { leftEye, rightEye };
+
+    }
+
+    void Init()
+    {
+        InitRenderTexture(ref this.targetLeft, this.leftEye);
+        InitRenderTexture(ref this.targetRight, this.rightEye);
+        InitEyeBuffer();
+    }
+
+    // Animate properties
+    void Update()
+    {
+        if (Application.isPlaying)
+        {
+            fractalPower += powerIncreaseSpeed * Time.deltaTime;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Init();
+        SetParameters();
+
+        this.fractalShader.GetKernelThreadGroupSizes(fractalKernalId, out uint xGroupSize, out uint yGroupSize, out uint zGroupSize);
+
+        int threadGroupsX = Mathf.CeilToInt(this.leftEye.pixelWidth / xGroupSize);
+        int threadGroupsY = Mathf.CeilToInt(this.leftEye.pixelHeight / yGroupSize);
+        int threadGroupsZ = Mathf.CeilToInt(this.eyes.Length / zGroupSize);
+        fractalShader.Dispatch(0, threadGroupsX, threadGroupsY, threadGroupsZ);
+
+        Graphics.Blit(this.targetLeft, destination);
+    }
+
+    void SetParameters()
+    {
+        fractalShader.SetBuffer(0, "eyes", this.eyeComputeBuffer);
+        fractalShader.SetTexture(0, "DestinationLeft", this.targetLeft);
+        fractalShader.SetTexture(0, "DestinationRight", this.targetRight);
+        fractalShader.SetFloat("power", Mathf.Max(fractalPower, 1.01f));
+        fractalShader.SetFloat("darkness", darkness);
+        fractalShader.SetFloat("blackAndWhite", blackAndWhite);
+        fractalShader.SetVector("colourAMix", new Vector3(redA, greenA, blueA));
+        fractalShader.SetVector("colourBMix", new Vector3(redB, greenB, blueB));
+        fractalShader.SetVector("_LightDirection", directionalLight.transform.forward);
+
+    }
+
+    void InitRenderTexture(ref RenderTexture texture, Camera cam)
+    {
+        if (texture == null || texture.width != cam.pixelWidth || texture.height != cam.pixelHeight)
+        {
+            if (texture != null)
+            {
+                texture.Release();
+            }
+            texture = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            texture.enableRandomWrite = true;
+            texture.Create();
+        }
+    }
+
+    void InitEyeBuffer()
+    {
+        if (this.eyeComputeBuffer == null || this.eyeComputeBuffer.count != this.eyes.Length)
+        {
+            if (this.eyeComputeBuffer != null)
+            {
+                this.eyeComputeBuffer.Release();
+            }
+            this.eyeComputeBuffer = new ComputeBuffer(this.eyes.Length, sizeof(float) * 2);
+            this.eyeComputeBuffer.SetData(this.eyes);
+        }
+    }
+
+
+}
